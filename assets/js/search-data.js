28679612a252a136f865cf5e168e3c6112d6ac99
liuxiaotong15 <tonyliu0401@126.com>
2025-05-22 14:04:39 +0000

    Deploying to gh-pages from @ liuxiaotong15/liuxiaotong15.github.io@2eeca78faf8ec3a05f3690c7311cb1e5877fabb6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -100,6 +100,11 @@ ninja.data = [{
           description: "2025年4月26-27日 山西 太原",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-体育竞技-篮球赛-冠军",
+          title: '体育竞技 篮球赛 冠军',
+          description: "2025年5月6日 北信科沙河",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
